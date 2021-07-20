9ed9e4c512c5981b113a199d2e94d17da492e442
kethrim <kethrim@ciencias.unam.mx>
2021-07-20 18:58:42 -0500

    Avances del método de pago

--- devolada/src/app/metodo-pago/metodo-pago.component.ts
+++ devolada/src/app/metodo-pago/metodo-pago.component.ts
@@ -9,12 +9,12 @@ export class MetodoPagoComponent implements OnInit {
 
   listaDir = [
     {calleNum:"Churbusco 152", colonia: "Metropolitana", ciudad: "Nezahualcoyotl",
-  estado: "México", cp: 78562 },
-  {calleNum:"Matemáticas 336", colonia: "Centro", ciudad: "Benito Juárez",
-  estado: "Ciudad de México", cp: 94523 }
+    estado: "México", cp: 78562 },
+    {calleNum:"Matemáticas 336", colonia: "Centro", ciudad: "Benito Juárez",
+    estado: "Ciudad de México", cp: 94523 }
   ];
 
-  
+  // Formato en el que se muestra una dirección 
   formato(i:number) : string {
     let dir = "";
     dir = this.listaDir[i].calleNum+", "+ this.listaDir[i].colonia+", "+this.listaDir[i].ciudad+", "+
