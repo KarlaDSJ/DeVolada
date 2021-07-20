7f0cfc49c7433f87696d2b542f2254ffcbd922f3
kethrim <kethrim@ciencias.unam.mx>
2021-07-20 17:58:36 -0500

    Ya puede elegir una dirección

--- devolada/src/app/metodo-pago/metodo-pago.component.ts
+++ devolada/src/app/metodo-pago/metodo-pago.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetodoPagoComponent implements OnInit {
 
+  listaDir = [
+    {calleNum:"Churbusco 152", colonia: "Metropolitana", ciudad: "Nezahualcoyotl",
+  estado: "México", cp: 78562 },
+  {calleNum:"Matemáticas 336", colonia: "Centro", ciudad: "Benito Juárez",
+  estado: "Ciudad de México", cp: 94523 }
+  ];
+
+  
+  formato(i:number) : string {
+    let dir = "";
+    dir = this.listaDir[i].calleNum+", "+ this.listaDir[i].colonia+", "+this.listaDir[i].ciudad+", "+
+    this.listaDir[i].estado+", "+this.listaDir[i].cp;
+    return dir;
+  }
+  
+
   constructor() { }
 
   ngOnInit(): void {
