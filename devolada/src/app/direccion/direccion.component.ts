1ccc71d6560d985580f4612b689385cffb240fbb
kethrim <kethrim@ciencias.unam.mx>
2021-07-21 21:32:42 -0500

    Las cree con los nombres al revés xD, solo los puse bien :)

--- devolada/src/app/direccion/direccion.component.ts
+++ devolada/src/app/direccion/direccion.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-direccion',
   templateUrl: './direccion.component.html',
   styleUrls: ['./direccion.component.css']
 })
+
 export class DireccionComponent implements OnInit {
+
+  // Cambiar por la consulta a la base de datos
+  listaDir = [
+    {calleNum:"Churbusco 152", colonia: "Metropolitana", ciudad: "Nezahualcoyotl",
+    estado: "México", cp: 78562 },
+    {calleNum:"Matemáticas 336", colonia: "Centro", ciudad: "Benito Juárez",
+    estado: "Ciudad de México", cp: 94523 }
+  ];
+  
+  validez=false;
+
+  // Formato en el que se muestra una dirección 
+  formato(i:number) : string {
+    let dir = "";
+    dir = this.listaDir[i].calleNum+", "+ this.listaDir[i].colonia+", "+this.listaDir[i].ciudad+", "+
+    this.listaDir[i].estado+", "+this.listaDir[i].cp;
+    return dir;
+  }
+  
+  // Valida los campos del formulario 
+  validarDir(f:NgForm){
+    this.validez= true;
+    if (f.invalid) {
+      return;
+    } 
+
+    this.listaDir.push({
+      calleNum: f.value.calle + " "+ f.value.num,
+      colonia: f.value.colonia,
+      ciudad: f.value.ciudad,
+      estado: f.value.estado,
+      cp: Number(f.value.cp)
+    });       
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
 }
+
+
+
--- devolada/src/app/metodo-pago/metodo-pago.component.ts
+++ devolada/src/app/metodo-pago/metodo-pago.component.ts
@@ -1,50 +1,17 @@
+
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-metodo-pago',
   templateUrl: './metodo-pago.component.html',
   styleUrls: ['./metodo-pago.component.css']
 })
+
 export class MetodoPagoComponent implements OnInit {
-
-  // Cambiar por la consulta a la base de datos
-  listaDir = [
-    {calleNum:"Churbusco 152", colonia: "Metropolitana", ciudad: "Nezahualcoyotl",
-    estado: "México", cp: 78562 },
-    {calleNum:"Matemáticas 336", colonia: "Centro", ciudad: "Benito Juárez",
-    estado: "Ciudad de México", cp: 94523 }
-  ];
-  
-  validez=false;
-
-  // Formato en el que se muestra una dirección 
-  formato(i:number) : string {
-    let dir = "";
-    dir = this.listaDir[i].calleNum+", "+ this.listaDir[i].colonia+", "+this.listaDir[i].ciudad+", "+
-    this.listaDir[i].estado+", "+this.listaDir[i].cp;
-    return dir;
-  }
-  
-  // Valida los campos del formulario 
-  validarDir(f:NgForm){
-    this.validez= true;
-    if (f.invalid) {
-      return;
-    } 
-
-    this.listaDir.push({
-      calleNum: f.value.calle + " "+ f.value.num,
-      colonia: f.value.colonia,
-      ciudad: f.value.ciudad,
-      estado: f.value.estado,
-      cp: Number(f.value.cp)
-    });       
-  }
 
   constructor() { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
   }
 
 }
