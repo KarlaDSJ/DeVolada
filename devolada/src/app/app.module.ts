ada76d6d15648fb693fde341cdaabaef5a5d1fcf
KarlaDSJ <karla_dsj@ciencias.unam.mx>
2021-07-22 15:31:49 -0500

    add: Componente Todos, Top5, producto y tarjeta producto, falta que sean responsivos, /inicio

--- devolada/src/app/app.module.ts
+++ devolada/src/app/app.module.ts
@@ -7,12 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { MetodoPagoComponent } from './metodo-pago/metodo-pago.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProductoComponent } from './producto/producto.component';
+import { TarjetaProductoComponent } from './tarjeta-producto/tarjeta-producto.component';
+import { MasVendidosComponent } from './mas-vendidos/mas-vendidos.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarritoComponent,
-    MetodoPagoComponent
+    MetodoPagoComponent,
+    InicioComponent,
+    ProductoComponent,
+    TarjetaProductoComponent,
+    MasVendidosComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
