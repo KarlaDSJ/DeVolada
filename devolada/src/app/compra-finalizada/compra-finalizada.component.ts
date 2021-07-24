f8acb2ae9ad7bcc493fe409d3c1329ce2e11bf50
kethrim <kethrim@ciencias.unam.mx>
2021-07-24 16:41:09 -0500

    Está casi lista :p

--- devolada/src/app/compra-finalizada/compra-finalizada.component.ts
+++ devolada/src/app/compra-finalizada/compra-finalizada.component.ts
@@ -13,6 +13,13 @@ export class CompraFinalizadaComponent implements OnInit {
   nombreP = "Nombre del producto";
   cant = 1;
 
+  listaP = [
+    {imagenp: "https://i.pinimg.com/originals/1a/ac/af/1aacaff36d04df6b1d189c6f22b4ceb9.jpg", 
+    nombre: "Shiba bb", cant: 1, idP: 12, precioP: 500, disp: 4},
+    {imagenp: "https://demascotas.info/wp-content/uploads/2018/01/dog-3098176_1280.jpg", 
+    nombre: "Shiba bb", cant: 1, idP: 13, precioP: 800, disp: 1}    
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
