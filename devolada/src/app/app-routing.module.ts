4c3feb1e2a889cb5dd887299f1477cfa7575b3ce
kethrim <kethrim@ciencias.unam.mx>
2021-07-23 21:38:02 -0500

    Faltaba una ,

--- devolada/src/app/app-routing.module.ts
+++ devolada/src/app/app-routing.module.ts
@@ -6,11 +6,8 @@ import { CarritoComponent } from './carrito/carrito.component';
 import { DireccionComponent } from './direccion/direccion.component';
 import { MetodoPagoComponent } from './metodo-pago/metodo-pago.component';
 import { MisProductosComponent } from './mis-productos/mis-productos.component';
-<<<<<<< HEAD
 import { CompraFinalizadaComponent } from './compra-finalizada/compra-finalizada.component';
-=======
 import { CrearResenaComponent } from './crear-resena/crear-resena.component';
->>>>>>> 86f12f7312af2fa3c1885fd7f828c7b32532aae2
 
 const routes: Routes = [
   {path: 'carrito', component: CarritoComponent},
@@ -19,11 +16,8 @@ const routes: Routes = [
   {path: 'producto', component: ProductoComponent},
   {path: 'direccion', component: DireccionComponent},
   {path: 'mis-productos', component: MisProductosComponent},
-<<<<<<< HEAD
-  {path: 'compra-finalizada', component: CompraFinalizadaComponent}
-=======
+  {path: 'compra-finalizada', component: CompraFinalizadaComponent},
   {path: 'crear-resena', component: CrearResenaComponent},
->>>>>>> 86f12f7312af2fa3c1885fd7f828c7b32532aae2
 ];
 
 @NgModule({
