54912bf2c089a05d079351fcb6c5d826f241b8ac
KarlaDSJ <karla_dsj@ciencias.unam.mx>
2021-07-28 17:02:52 -0500

    Add: Blueprints (ej. Agregar un producto)

---
+++ backend/blueprints/productos.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+from models.productoM import Producto
+from schemas.productoE import ProductoEsquema
+from main import db
+
+producto = Blueprint('producto', __name__)
+
+producto_esquema = ProductoEsquema()
+productos_esquema = ProductoEsquema(many=True)
+
+@producto.route('/', methods=['GET'])
+def get():
+    return jsonify ({'msg': 'Bienvenido a DeVolada'})
+
+@producto.route('/producto', methods=['POST'])
+def agrega_producto():
+    correo = request.json['correo']
+    precio = request.json['precio']
+    nombre = request.json['nombre']
+    descripcion = request.json['descripcion']
+    vendidos = request.json['vendidos']
+    disponible = request.json['disponible']
+
+    producto_nuevo = Producto(correo, precio, nombre, descripcion, vendidos, disponible)
+
+    db.session.add(producto_nuevo)
+    db.session.commit()
+
+    return producto_esquema.jsonify(producto_nuevo)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 ma = Marshmallow()
-from models.productoM import Producto
-from schemas.productoE import ProductoEsquema
-
+from blueprints import productos
 
 app = Flask(__name__)
 
@@ -15,29 +13,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 ma.init_app(app)
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify ({'msg': 'Bienvenido a DeVolada'})
 
-
-producto_esquema = ProductoEsquema()
-productos_esquema = ProductoEsquema(many=True)
-
-@app.route('/producto', methods=['POST'])
-def agrega_producto():
-    correo = request.json['correo']
-    precio = request.json['precio']
-    nombre = request.json['nombre']
-    descripcion = request.json['descripcion']
-    vendidos = request.json['vendidos']
-    disponible = request.json['disponible']
-
-    producto_nuevo = Producto(correo, precio, nombre, descripcion, vendidos, disponible)
-
-    db.session.add(producto_nuevo)
-    db.session.commit()
-
-    return producto_esquema.jsonify(producto_nuevo)
+app.register_blueprint(productos.producto)
 
 
 if __name__ == '__main__':
