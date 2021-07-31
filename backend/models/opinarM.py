dacc1e7392e11a8565f67e0181a25bbf2b15b1ac
antonioreyesmtz21 <antonioreyes21@ciencias.unam.mx>
2021-07-30 20:12:45 -0500

    Agregué Modelo opinarM

--- backend/models/opinarM.py
+++ backend/models/opinarM.py
@@ -1,8 +1,13 @@
 from main import db
 
-class Opinar(db.model):
+class Opinar(db.Model):
     __tablename__ = 'opinar'
-    idProducto = db.Column(str(45),primary_key = True, unique=True)
+    idProducto = db.Column(db.String(45),primary_key = True, unique=True)
+    correo = db.Column(db.String(45))
+    opinion = db.Column(db.Unicode)
+    calificacion = db.Column(db.Integer)
 
-
-
+    def __init__(self, correo, opinion, calificacion):
+        self.correo = correo
+        self.opinion = opinion
+        self.calificacion = calificacion
