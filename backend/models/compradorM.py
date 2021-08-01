e2a78ebcfcb6e1979ef5ae9abb75a4a1b4b1efc5
kethrim <kethrim@ciencias.unam.mx>
2021-07-31 20:42:28 -0500

    Comenté las relaciones que tiene comprador

--- backend/models/compradorM.py
+++ backend/models/compradorM.py
@@ -8,11 +8,11 @@ class Comprador(db.Model):
     nombre = db.Column(db.Unicode)
     telefono = db.Column(db.String(15))
     contrasenia = db.Column(db.String(105))
-    # Relaciones
-    compras = db.relationship('Compra', backref='comprador', lazy=True) # Relación entre Compra y Comprador    
-    carrito_propio = db.relationship('Pertenecer', backref='comprador', lazy=True)
-    productos_carrito = db.relationship('Contener', backref='comprador', lazy=True) 
-    productos_compra = db.relationship('Incluir', backref='comprador', lazy=True)
+    # # Relaciones
+    # compras = db.relationship('Compra', backref='comprador', lazy=True) # Relación entre Compra y Comprador    
+    # carrito_propio = db.relationship('Pertenecer', backref='comprador', lazy=True)
+    # productos_carrito = db.relationship('Contener', backref='comprador', lazy=True) 
+    # productos_compra = db.relationship('Incluir', backref='comprador', lazy=True)
 
     def __init__(self,correo, nombre,telefono,contrasenia):
         self.correo = correo
