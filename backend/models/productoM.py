7b200463700e1724ea9bf65e4837b5e5f439ca01
kethrim <kethrim@ciencias.unam.mx>
2021-07-31 21:43:59 -0500

    Agregué la relación entre producto y vendedor. Debe de ir en vendedor, no en producto

--- backend/models/productoM.py
+++ backend/models/productoM.py
@@ -1,18 +1,16 @@
 from main import db
 from sqlalchemy import ForeignKey
-from sqlalchemy import relationship
+# from sqlalchemy import relationship
 
 class Producto(db.Model):
     """Modelo de la tabla Producto de la BD"""
-    id_producto = db.Column(db.Integer, primary_key=True)
-    correo = db.Column(db.String(45), ForeignKey('vendedor.correo'))
+    idProducto = db.Column(db.Integer, primary_key=True, unique=True)
+    correo = db.Column(db.String(45), db.ForeignKey('vendedor.correo'), nullable=False)
     precio = db.Column(db.Float)
     nombre = db.Column(db.String(45))
     descripcion = db.Column(db.String(100)) #Creo que 100 es poco
     vendidos = db.Column(db.Integer)
-    disponibles = db.Column(db.Integer)
-
-    producto = relationship("vendedor")
+    disponibles = db.Column(db.Integer)    
 
 
     def __init__(self, correo, precio, nombre, descripcion, vendidos, disponible):
