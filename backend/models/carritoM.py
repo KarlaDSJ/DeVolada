8502b7d54616fec91b72deb0e2e346dce56ae687
kethrim <kethrim@ciencias.unam.mx>
2021-07-31 23:56:40 -0500

    Cree la entidad carrito :)

--- backend/models/carritoM.py
+++ backend/models/carritoM.py
@@ -1,13 +1,8 @@
 from main import db
 from enum import unique
 
-
 # Carrito de compras 
 class Carrito (db.Model):
-    idCarrito = db.Column(db.Integer, primary_key=True,  unique=True)
+    idCarrito = db.Column(db.Integer, primary_key=True, unique=True)
     # Relaciones
-    productos = db.relationship('Pertenecer', backref='comprador', lazy=True)
-
-
-    def __init__ (self):
-        pass
+    # dueno = db.relationship('Pertenecer', backref='comprador', lazy=True)        
