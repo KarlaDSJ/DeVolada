eb94005a471f24251bd03554f78141b099a40fbe
kethrim <kethrim@ciencias.unam.mx>
2021-07-31 20:41:51 -0500

    Solo Comprador Agregué mis blueprints y comenté lo de Omar

--- backend/main.py
+++ backend/main.py
@@ -2,48 +2,62 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-
 db = SQLAlchemy()
 ma = Marshmallow()
 
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
-from blueprints.productos import producto
 from blueprints.index import index
 from blueprints.login import login
 from blueprints.create import create
 from blueprints.logout import logout
-#from blueprints.before import before
 from blueprints.after import after
 from blueprints.cookie import cookie
-from blueprints.adminProducto import adminProducto
+#from blueprints.before import before
+# from blueprints.adminProducto import adminProducto
+# from blueprints.productos import producto
+
+# Mis blueprints
+from blueprints.comprador import comprador
 
 app = Flask(__name__)
 csrf = CSRFProtect()
 
 app.config.from_object(DevelopmentConfig)
 
+app.register_blueprint(comprador)
 
+# Ya estaban 
 app.register_blueprint(index)
 app.register_blueprint(login)
 app.register_blueprint(create)
 app.register_blueprint(logout)
-#app.register_blueprint(before)
 app.register_blueprint(after)
 app.register_blueprint(cookie)
-app.register_blueprint(adminProducto)
+#app.register_blueprint(before)
+# app.register_blueprint(adminProducto)
+
+
+# Configuraciones para envíos de correos en compra
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'mercadodevolada@gmail.com'
+app.config['MAIL_PASSWORD'] = '7yR9Y5iDhrmXKjG'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 
 db.init_app(app)
-csrf.init_app(app)
-app.run(port=7000)
+# Lo comenté porque necesitaba iniciar sesión para hacer peticiones
+# csrf.init_app(app)
 
 
 
 
 
 if __name__ == '__main__':
-    app.run(port=7000)
+    app.run(port=7000)    
+
     with app.app_context():
         db.create_all()
         ma.init_app(app)
