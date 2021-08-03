8d5e18a9ddf663fec29db91570b283611ec8d93f
antonioreyesmtz21 <antonioreyes21@ciencias.unam.mx>
2021-08-03 16:55:59 -0500

    Agregué services de Reseña

---
+++ frontend/src/app/resenas.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {HttpParams} from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+export interface IResena {
+  idProducto: String;
+  correo: String;
+  calificacion: number;
+  opinion: String;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResenasService {
+  private _url = "http://127.0.0.1:5000";
+
+  constructor(private _http: HttpClient) { }
+
+  /*
+    Nos regresa todos los productos
+  */
+  crearResenas(idProducto:string, correo:string, calificacion:number, opinion:string) : Observable<IResena[]>{
+    const params = new HttpParams()
+    .set('idProducto', idProducto)
+    .set('correo', correo)
+    .set('calificacion', calificacion)
+    .set('opinion', opinion)
+    return this._http.post<IResena[]>(this._url+"/crearResena",  {params})
+  }
+
+   /*
+    Nos regresa la informacion de un producto
+  */
+    mostrarResenas(idProducto:string): Observable<IResena[]>{
+        const params = new HttpParams().set('idProducto', idProducto);
+        return this._http.get<IResena[]>(this._url+"/mostrarResenas", { params: params });
+    }
+
+}
