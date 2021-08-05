0a36298e041fa101ee1502ad3ead4de68935d2c2
kethrim <kethrim@ciencias.unam.mx>
2021-08-04 19:22:50 -0500

    Agregué documentación y cree una interfaz para la creación del carrito

--- frontend/src/app/carrito.service.ts
+++ frontend/src/app/carrito.service.ts
@@ -26,6 +26,11 @@ export interface IContenerProducto{
   cantidad: number
 }
 
+export interface ICarritoComprador{
+  idCarrito: number,
+  correo: string
+}
+
 export interface IMensaje{
   msg: string
 }
@@ -54,32 +59,89 @@ export class CarritoService {
 
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
+  // PROBAR
+  /**
+   * Crea un carrito 
+   * @returns Interfaz de un carrito 
+   */
   crearCarrito(): Observable<ICarrito>{
     return this._http.post<ICarrito>(this._url+"/carrito",{})
   }
 
+  // PROBAR
+  /**
+   * Asigna un carrito a un comprador
+   * @param idCarrito identificador del carrito
+   * @param correo correo del comprador al que se le asignará el carrito  
+   * @returns Correo del comprador y id del carrito con la interfaz ICarritoComprador
+   */
+  asignarCarrito(idCarrito:number, correo:string): Observable<ICarritoComprador>{
+    return this._http.post<ICarritoComprador>(`${this._url}/pertenecer`, {idCarrito, correo})
+  }
+
+  /**
+   * Agrega un producto a un carrito  
+   * @param idProducto identificador del producto a agregar 
+   * @param idCarrito identificador del carrito en el que se agregará
+   * @returns Mensaje indicando si se puedo o no agregar
+   */
   agregarCarrito(idProducto:number, idCarrito:number): Observable<IMensaje>{
     return this._http.post<IMensaje>(`${this._url}/contener`, {idProducto, idCarrito})
   }
 
+  /**
+   * Obtiene los productos de un carrito
+   * @param idCarrito identificador del carrito
+   * @returns Lista de productos que contiene el carrito
+   */
   obtenerProductos(idCarrito:number) : Observable<IProductoCarrito[]>{
     return this._http.get<IProductoCarrito[]>(`${this._url}/contener?idCarrito=${idCarrito}`)
   }
 
+  /**
+   * Cambia la cantidad de un producto en un carrito. 
+   * El producto ya debe de estar en el carrito. 
+   * En caso de no poder cambiarla mantiene la cantidad anterior.
+   * @param idProducto identificador del producto
+   * @param idCarrito identificador del carrito
+   * @param cantidad nueva cantidad del producto en el carrito
+   * @return Información usando la interfaz IContenerProducto
+   */
   cambiarCantidad(idProducto:number, idCarrito:number, cantidad:number): Observable<IContenerProducto>{
     const params={idProducto, idCarrito} 
     return this._http.put<IContenerProducto>(`${this._url}/contener`,{cantidad}, {params})
   }
+
+  /**
+   * Elimina un producto de un carrito
+   * @param idProducto identificador del producto
+   * @param idCarrito identificador del carrito
+   * @returns Mensaje indicando si se pudo eliminar o no
+   */
 
   eliminarProducto(idProducto:number, idCarrito:number): Observable<IMensaje>{
     const params={idProducto, idCarrito} 
     return this._http.delete<IMensaje>(`${this._url}/contener`, {params})
   }
 
+  /**
+   * Obtiene el total acumulado por los productos y cantidades de un carrito
+   * @param idCarrito identificador del carrito
+   * @returns Total del carrito
+   */
   obtenerTotal(idCarrito:number): Observable<number> {
     return this._http.get<number>(`${this._url}/totalCarrito?idCarrito=${idCarrito}`)
   }
   
+  /**
+   * Elimina todos los productos y sus respectivas cantidades de un carrito
+   * @param idCarrito identificador del carrito
+   * @returns Mensaje indicando si se pudo o no limpiar el carrito
+   */
+  limpiarCarrito(idCarrito:number): Observable<IMensaje>{
+    return this._http.delete<IMensaje>(`${this._url}/limpiarCarrito?idCarrito=${idCarrito}`)
+  }
+
   /*
     Registra una compra en la base de datos 
   */
@@ -102,8 +164,5 @@ export class CarritoService {
   productosComprados(idCompra:number): Observable<IIncluir[]>{
     return this._http.get<IIncluir[]>(this._url+"/compra/"+idCompra);
   }
-
-  limpiarCarrito(idCarrito:number): Observable<IMensaje>{
-    return this._http.delete<IMensaje>(`${this._url}/limpiarCarrito?idCarrito=${idCarrito}`)
-  }
+  
 }
