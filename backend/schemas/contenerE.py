20ea9a0d933fd765b86fed2ea6c68917ee28e908
kethrim <kethrim@ciencias.unam.mx>
2021-08-10 18:50:48 -0500

    Documentación de los esquemas

--- backend/schemas/contenerE.py
+++ backend/schemas/contenerE.py
@@ -1,5 +1,9 @@
 from main import ma
 
 class ContenerEsquema(ma.Schema):
+    """ Crea un esquema para la relación contener
+    que incluye el identificador del producto,
+    el del carrito y la cantidad que se incluye
+    """
     class Meta:
         fields = ('idProducto', 'idCarrito', 'cantidad')
