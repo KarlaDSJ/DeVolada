35c6dbdb51db79bd05bfda5169f1574fa2397365
marcoantonio1999 <marcoorduna@ciencias.unam.mx>
2021-08-11 06:39:42 -0500

    auth comprador y componente login comentados

--- frontend/src/app/modules/login/login.component.ts
+++ frontend/src/app/modules/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ILogin } from '../../services/login.service';
 import { LoginService } from "../../services/login.service";
-import { ActivatedRoute } from '@angular/router';
 import { Router}   from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import Swal from 'sweetalert2';
@@ -12,92 +11,109 @@ import Swal from 'sweetalert2';
   styleUrls: ['./login.component.css']
 })
 
+/**
+ * Clase para el funcionamiento del componente Login del comprador y del vendedor
+ */
+
 export class LoginComponent implements OnInit {
+
+  // Datos que vamos a ocupar, solo las inicializamos
 
   login : ILogin;
   datos = '';
   sesion = '';
   nombre = '';
+   
+  //Diccionario con datos del comprador
+
   dataC ={
   correoC:'',
   contraseniaC:''
   }
 
+  //Diccionario con datos del vendedor
+
   dataV ={
     correoV:'',
     contraseniaV:''
     }
-  
-  constructor(private _route:ActivatedRoute,
-    private _loginService: LoginService, 
-    private router: Router,
-    private cookieServive: CookieService){}
-    
 
-  ngOnInit(): void {
-   
- }
+  /**
+   * Constructor de la clase
+   * @param _loginService varible del servicio del login para poder subsribirse a este servicio
+   * @param router uan rata para dirigir a otra ruta si es necesario
+   * @param cookieServive la cookie que manda el servidor para mantener la sesión
+   */
+
+  constructor(  private _loginService: LoginService, 
+                private router: Router,
+                private cookieServive: CookieService){}
     
+  /**
+   * Constructor que se incializa cada que se carga el componente
+   */
+
+  ngOnInit(): void {}
+    
+  /**
+   * Funcion que se subscribe al servicio de login para enviar los datos del
+   * comprador, si los datos no son correctos se manda mensaje. 
+   */
 
   submitC(){
-    //console.log(this.data)
-  this._loginService.IniciarSesionC(this.dataC).subscribe(response => {
-    console.log(response)
-    this.datos = response.msg;
-    if (this.datos == 'error_datos'){
-      Swal.fire({icon:'error',
-      title: 'oooops',
-      text: 'Los datos ingresados son incorectos'})
-    }
-    if(this.datos == 'error_contrasenia'){
-      Swal.fire({icon:'error',
-      title: 'oooops',
-      text: 'La contraseña es incorrecta'})
-    }
-    if(this.datos == 'success'){       
-      this.sesion = response.session;
-      this.nombre = response.nombre;
-      Swal.fire({icon:'success',
-      title: 'Bienvenido de nuevo',
-      text: this.nombre} )
+    this._loginService.IniciarSesionC(this.dataC).subscribe(response => { //nos subscribimos
+      this.datos = response.msg;
+      if (this.datos == 'error_datos'){
+        Swal.fire({icon:'error',
+        title: 'oooops',
+        text: 'Los datos ingresados son incorectos'})
+      }
+      if(this.datos == 'error_contrasenia'){
+        Swal.fire({icon:'error',
+        title: 'oooops',
+        text: 'La contraseña es incorrecta'})
+      }
+      if(this.datos == 'success'){       
+        this.sesion = response.session;
+        this.nombre = response.nombre;
 
-      this.cookieServive.set('token_accessC',response.session,1,'/')
-      this.router.navigate(['/inicio']);
-      
-    }
+        Swal.fire({icon:'success',
+        title: 'Bienvenido de nuevo',
+        text: this.nombre})
+
+        this.cookieServive.set('token_accessC',response.session,1,'/')
+        this.router.navigate(['/inicio']);
+      }
     })
-    
   }
+
+   /**
+   * Funcion que se subscribe al servicio de login para enviar los datos del
+   * vendedor, si los datos no son correctos se manda mensaje. 
+   */
 
   submitV(){
-    //console.log(this.data)
-  this._loginService.IniciarSesionV(this.dataV).subscribe(response => {
-    this.datos = response.msg;
-    if (this.datos == 'error_datos'){
-      Swal.fire({icon:'error',
-      title: 'oooops',
-      text: 'Los datos ingresados son incorectos'})
-    }
-    if(this.datos == 'error_contrasenia'){
-      Swal.fire({icon:'error',
-      title: 'oooops',
-      text: 'La contraseña es incorrecta'})
-    }
-    if(this.datos == 'success'){
-      this.sesion = response.session;
-      this.nombre = response.nombre;
-      Swal.fire({icon:'success',
-      title: 'Bienvenido de nuevo',
-      text: this.nombre} )
-      this.router.navigate(['/mis-productos']);
-      this.cookieServive.set('token_accessV',response.session,1,'/')
-    }
-  })
+    this._loginService.IniciarSesionV(this.dataV).subscribe(response => {
+      this.datos = response.msg;
+      if (this.datos == 'error_datos'){
+        Swal.fire({icon:'error',
+        title: 'oooops',
+        text: 'Los datos ingresados son incorectos'})
+      }
+      if(this.datos == 'error_contrasenia'){
+        Swal.fire({icon:'error',
+        title: 'oooops',
+        text: 'La contraseña es incorrecta'})
+      }
+      if(this.datos == 'success'){
+        this.sesion = response.session;
+        this.nombre = response.nombre;
+        Swal.fire({icon:'success',
+        title: 'Bienvenido de nuevo',
+        text: this.nombre} )
+        this.router.navigate(['/mis-productos']);
+        this.cookieServive.set('token_accessV',response.session,1,'/')
+      }
+    })
   }
 }
-
-
-
-
-
-
