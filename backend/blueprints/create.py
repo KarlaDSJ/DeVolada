15370f5f16aaa66be73497a25d7b135fd1eeca8c
marcoantonio1999 <marcoorduna@ciencias.unam.mx>
2021-08-14 19:53:59 -0500

    correos bonitos

--- backend/blueprints/create.py
+++ backend/blueprints/create.py
@@ -13,6 +13,7 @@ from schemas.direccionE import DireccionEsquema
 from random import choice
 from flask import current_app as app
 from flask_mail import Message,Mail
+from flask import render_template
 import re
 
 create = Blueprint('create',__name__)
@@ -44,14 +45,14 @@ def agrega_comprador():
     
     
     longitud = 8
-    valores = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<=>@#%&+"
+    valores = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+"
     contraseniaRandom = ""
     contraseniaRandom = contraseniaRandom.join([choice(valores) for i in range(longitud)])
     comprador = Comprador.query.filter_by(correo = correo).first()
     
     if(comprador is None):
         if(verificar_arroba(correo)):
-            enviar_correo(correo, nombre, contraseniaRandom)
+            enviar_correoC(correo, nombre, contraseniaRandom)
             direccion_nueva = DireccionComprador(correo, estado, ciudad, colonia, cp, calle, numero)
             comprador_nuevo = Comprador(correo, nombre, telefono, contraseniaRandom)
             db.session.add(comprador_nuevo)
@@ -88,7 +89,7 @@ def agrega_vendedor():
     vendedor = Vendedor.query.filter_by(correo = correo).first()
     if(vendedor is None):
         if(verificar_arroba(correo)):
-            enviar_correo(correo, nombre, contraseniaRandom)
+            enviar_correoV(correo, nombre, contraseniaRandom)
             direccion_nueva = DireccionVendedor(correo, estado, ciudad, colonia, cp, calle, numero)
             vendedor_nuevo = Vendedor(correo, nombre, telefono, contraseniaRandom)
             tarjeta_nueva = TarjetaVendedor(correo, tarjeta) 
@@ -105,13 +106,87 @@ def agrega_vendedor():
         return jsonify({'msg':'correo_registrado'})
         
     
-def enviar_correo(correo, nombre, contrasenia):
+def enviar_correoC(correo, nombre, contrasenia):
     mail = Mail(app)
     try:
 
         mensaje = Message('DeVolada',sender= 'marcoordunaavila@gmail.com', recipients = [correo])
-        mensaje.body = f"Confirmación de corrreo \n  Hola {nombre} \n Esta es tu contraseña con la que puedes acceder al sistema: {contrasenia} \n"
+        mensaje.body = "holiwis"
+
         
+        mensaje.html = f'''<!doctype html> <html lang="en">   <head>    <!-- Required meta tags -->    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1">    <!-- Bootstrap CSS -->    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">   <title>Hello, world!</title>  </head>  <body>    
+        <div class="card text-center">
+        <h1>DeVolada.com</h1>
+        <a href="https://imgbb.com/"><img src="https://i.ibb.co/BtGkC8p/DV.png" alt="DV" border="0"></a>
+        <div class="card-header" style="margin-top: 10px;">
+          Gracias por registrarte  {nombre} como comprador
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Esta es la contraseña con la que podras acceder al sistema: </h5> 
+          <h3>{contrasenia} </h3>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">DeVolada</a>
+        </div>
+        <div class="card-footer text-muted">
+          
+        </div>
+         </div>
+    
+    
+         <!-- Option 1: Bootstrap Bundle with Popper -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+         <!-- Option 2: Separate Popper and Bootstrap JS -->
+         <!--
+         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+            -->
+         </body>
+         </html>'''
+    
+        mail.send(mensaje)
+    except Exception:
+        raise
+    return "Correo enviado :)"
+
+def enviar_correoV(correo, nombre, contrasenia):
+    mail = Mail(app)
+    try:
+
+        mensaje = Message('DeVolada',sender= 'marcoordunaavila@gmail.com', recipients = [correo])
+        mensaje.body = "holiwis"
+
+        
+        mensaje.html = f'''<!doctype html> <html lang="en">   <head>    <!-- Required meta tags -->    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1">    <!-- Bootstrap CSS -->    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">   <title>Hello, world!</title>  </head>  <body>    
+        <div class="card text-center">
+        <h1>DeVolada.com</h1>
+        <a href="https://imgbb.com/"><img src="https://i.ibb.co/BtGkC8p/DV.png" alt="DV" border="0"></a>
+        <div class="card-header" style="margin-top: 10px;">
+          Gracias por registrarte  {nombre} como vendedor
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Esta es la contraseña con la que podras acceder al sistema: </h5> 
+          <h3>{contrasenia} </h3>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">DeVolada</a>
+        </div>
+        <div class="card-footer text-muted">
+          
+        </div>
+         </div>
+    
+    
+         <!-- Option 1: Bootstrap Bundle with Popper -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+         <!-- Option 2: Separate Popper and Bootstrap JS -->
+         <!--
+         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+            -->
+         </body>
+         </html>'''
+    
         mail.send(mensaje)
     except Exception:
         raise
